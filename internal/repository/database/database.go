54b5dc01ccf19fc1df9dc1f52a26c6c87b67edca
chloe cheng <chloecheng@chloedeMacBook-Pro.local>
2023-03-11 04:35:56 +0800

    rename db interface name

--- internal/repository/database/database.go
+++ internal/repository/database/database.go
@@ -13,35 +13,35 @@ type IDatabase interface {
 	Rollback() error
 	Transaction(ctx context.Context, callback func(ctx context.Context, txRepo IDatabase) error) error
 
-	IProduct
-	IPromotions
-	IMember
-	IOrder
-	IWallet
+	IProductDB
+	IPromotionDB
+	IMemberDB
+	IOrderDB
+	IWalletDB
 }
 
-type IPromotions interface {
+type IPromotionDB interface {
 	// ListPromotions 取得多筆優惠活動
 	ListPromotions(ctx context.Context, options *query.PromotionOptions) ([]*model.Promotion, error)
 	CreatePromotion(ctx context.Context, mPromotion *model.Promotion) error
 }
 
-type IProduct interface {
+type IProductDB interface {
 	ListProducts(ctx context.Context, options *query.ProductOptions) ([]*model.Product, error)
 	UpdateProduct(ctx context.Context, options *query.ProductOptions, updates *updates.Product) error
 }
 
-type IMember interface {
+type IMemberDB interface {
 	// GetMember 取得用戶的會員等級
 	GetMember(ctx context.Context, options *query.MemberOptions) (*model.Member, error)
 }
 
-type IOrder interface {
+type IOrderDB interface {
 	// CreateOrder 建立訂單
 	CreateOrder(ctx context.Context, order *model.Order) error
 }
 
-type IWallet interface {
+type IWalletDB interface {
 	// GetWallet 取得用戶的錢包
 	GetWallet(ctx context.Context, options *query.WalletOptions) (*model.Wallet, error)
 	// UpdateWallet 更新用戶的錢包
